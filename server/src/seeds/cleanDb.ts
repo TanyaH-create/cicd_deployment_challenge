a31ae3d2392de45378a7ffab6cd7de166708ae69
Tanya <dougtanyah@gmail.com>
2025-03-20 18:41:01 -0400

    chore: inital commit of given project

---
+++ server/src/seeds/cleanDb.ts
@@ -0,0 +1,43 @@
+import connection from '../config/connection.js';
+
+export default async (_modelName: string, collectionName: string) => {
+  try {
+    // Check if the collection exists using the connection directly
+    const db = connection.db;
+    
+    if (!db) {
+      throw new Error('Database connection not established');
+    }
+    
+    const modelExists = await db
+      .listCollections({ name: collectionName })
+      .toArray();
+
+    if (modelExists.length) {
+      await connection.dropCollection(collectionName);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+
+// import models from '../models/index.js';
+// import db from '../config/connection.js';
+
+// export default async (modelName: "Question", collectionName: string) => {
+//   try {
+//     let modelExists = await models[modelName].db.db.listCollections({
+//       name: collectionName
+//     }).toArray()
+
+//     if (modelExists.length) {
+//       await db.dropCollection(collectionName);
+//     }
+//   } catch (err) {
+//     throw err;
+//   }
+// }
+
+//Given code did not work - corrected code:
